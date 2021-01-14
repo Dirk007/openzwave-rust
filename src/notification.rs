fc77e8a8cfd3660b1bf5471592d3cdfe657eb9f8
Dirk Faust <dif@crosscan.com>
2021-01-14 08:11:02 +0100

    Be more "rustic"

--- src/notification.rs
+++ src/notification.rs
@@ -8,18 +8,23 @@ use node::Node;
 use controller::Controller;
 
 #[derive(Debug)]
+pub enum NotificationValue {
+    Group(u8),
+    Button(u8),
+    Scene(u8),
+    State(ControllerState),
+    Report(NotificationCode),
+}
+
+#[derive(Debug)]
 pub struct Notification {
     ptr: *const ExternNotification,
     pub notification_type: NotificationType,
     pub home_id: u32,
     pub node_id: u8,
     pub value_id: Option<ValueID>,
-    pub group_idx: Option<u8>,
+    pub value: Option<NotificationValue>,
     pub event: Option<u8>,
-    pub button_id: Option<u8>,
-    pub scene_id: Option<u8>,
-    pub controller_state: Option<ControllerState>,
-    pub notification_code: Option<NotificationCode>,
 }
 
 impl Into<String> for Notification {
@@ -65,29 +70,29 @@ impl Notification {
                             }
                     }
             },
-            group_idx: match notification_type {
-                NotificationType::Group =>
-                    Some(unsafe { extern_notification::notification_get_group_idx(ptr) }),
-                _ => None
+            value: match notification_type {
+                NotificationType::Group => Some(NotificationValue::Group(unsafe { extern_notification::notification_get_group_idx(ptr) })),
+                NotificationType::CreateButton | NotificationType::DeleteButton | NotificationType::ButtonOn | NotificationType::ButtonOff => Some(NotificationValue::Button(unsafe { extern_notification::notification_get_button_id(ptr) })),
+                NotificationType::SceneEvent => Some(NotificationValue::Scene(unsafe { extern_notification::notification_get_scene_id(ptr) })),
+                // TODO: This is wrong? m_command instead of m_byte used in openzwave-sys/open-zwave/cpp/src/Notification.h
+                NotificationType::ControllerCommand => {
+                    if let Some(state) = ControllerState::from_u8(unsafe { extern_notification::notification_get_notification(ptr) }) {
+                        Some(NotificationValue::State(state))
+                    } else {
+                        None
+                    }
+                },
+                NotificationType::Notification => {
+                    if let Some(code) = NotificationCode::from_u8(unsafe { extern_notification::notification_get_notification(ptr) }) {
+                        Some(NotificationValue::Report(code))
+                    } else {
+                        None
+                    }
+                },
+                _ => None,
             },
             event: match notification_type {
                 NotificationType::NodeEvent | NotificationType::ControllerCommand => Some(unsafe { extern_notification::notification_get_event(ptr) }),
-                _ => None
-            },
-            button_id: match notification_type {
-                NotificationType::CreateButton | NotificationType::DeleteButton | NotificationType::ButtonOn | NotificationType::ButtonOff => Some(unsafe { extern_notification::notification_get_button_id(ptr) }),
-                _ => None
-            },
-            scene_id: match notification_type {
-                NotificationType::SceneEvent => Some(unsafe { extern_notification::notification_get_scene_id(ptr) }),
-                _ => None
-            },
-            controller_state: match notification_type {
-                NotificationType::ControllerCommand => ControllerState::from_u8(unsafe { extern_notification::notification_get_notification(ptr) }),
-                _ => None
-            },
-            notification_code: match notification_type {
-                NotificationType::Notification => NotificationCode::from_u8(unsafe { extern_notification::notification_get_notification(ptr) }),
                 _ => None
             },
         };
